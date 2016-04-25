94a66e4e044d571c04aa592bff90ca755d329926
momo98 <athens.fitzcheung@gmail.com>
2016-04-25 20:20:51 +0000

    added activation migration

---
+++ db/migrate/20160425201652_add_activation_to_users.rb
@@ -0,0 +1,10 @@
+class AddActivationToUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :activated
+    remove_column :users, :activated_at
+    remove_column :users, :activation_digest
+    add_column :users, :activated, :boolean, default: false
+    add_column :users, :activated_at, :datestime
+    add_column :users, :activation_digest, :string
+  end
+end
