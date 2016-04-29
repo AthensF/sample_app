302573a5832c2a255fe6261292819ca6cf43aac8
momo98 <athens.fitzcheung@gmail.com>
2016-04-29 02:21:18 +0000

    adj db

--- db/migrate/20160425201652_add_activation_to_users.rb
+++ db/migrate/20160425201652_add_activation_to_users.rb
@@ -1,8 +1,8 @@
 class AddActivationToUsers < ActiveRecord::Migration
   def change
-    remove_column :users, :activated
-    remove_column :users, :activated_at
-    remove_column :users, :activation_digest
+    # remove_column :users, :activated
+    # remove_column :users, :activated_at
+    # remove_column :users, :activation_digest
     add_column :users, :activated, :boolean, default: false
     add_column :users, :activated_at, :datestime
     add_column :users, :activation_digest, :string
