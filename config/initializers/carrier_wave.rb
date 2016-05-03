7591789314cb0fbda5bf02d86804200d9a3ae5c4
momo98 <athens.fitzcheung@gmail.com>
2016-05-03 19:01:28 +0000

    adj 1

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
-    CarrierWave.configure do |config|
-        config.fog_credentials = {
-            #config for amazon S3
-            :provider       => 'AWS',
-            :aws_access_key_id  => ENV['AKIAICTWG6DY2XSGCELQ'],
-            :aws_secret_access_key  => ENV['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']
-        }
-        config.fog_directory        = ENV['momo98-rails-tutorial']
-    end
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
